c08f7adb9853ba76412d4773056e5669d1259456
Aniket-508 <pawaraniket508@gmail.com>
2026-06-04 15:41:54 +0530

    chore: removed redundant editframe api

--- lib/editframe.ts
+++
@@ -1,7 +0,0 @@
-import { Client } from "@editframe/api";
-
-if (!process.env.EF_API_TOKEN) {
-  throw new Error("Missing EF_API_TOKEN environment variable");
-}
-
-export const editframeClient = new Client(process.env.EF_API_TOKEN);
